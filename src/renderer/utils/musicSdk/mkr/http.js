30612e8b3a526e0e7f12e2c131494fe105ca82a0
Meng KR <mengkerun@gmail.com>
2025-11-18 18:54:08 +0800

    增加post请求

--- src/renderer/utils/musicSdk/mkr/http.js
+++ src/renderer/utils/musicSdk/mkr/http.js
@@ -24,12 +24,9 @@ export const get = (path, params = {}) => {
 
   const requestObj = httpFetch(url.toString(), options)
 
-  // 我们返回这个 promise
   return requestObj.promise.then(resp => {
-    // request.js (50行) 已经帮我们 JSON.parse(resp.body) 了
-    // 我们只需要检查 API 业务代码
+    // request.js (50行) 已经 JSON.parse(resp.body) 了
     if (resp.body.code !== 200) {
-      // API 逻辑失败 (例如 code: 404)
       throw new Error(resp.body.message || 'API 请求失败')
     } else {
       // API 成功, 返回 data 字段
@@ -38,6 +35,35 @@ export const get = (path, params = {}) => {
   }).catch(err => {
     // httpFetch 内部的 catch (116行) 会处理 ETIMEDOUT 等
     // 如果 .promise reject, 我们也将其抛出
+    return Promise.reject(err)
+  })
+}
+
+export const post = (path, data = {}) => {
+  const url = new URL(BASE_URL + path)
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'X-API-Key': API_KEY,
+      'Content-Type': 'application/json',
+    },
+    // request.js 会自动 stringify 它 (因为 json: true)
+    body: data,
+  }
+  console.log('options: ', options)
+  const requestObj = httpFetch(url.toString(), options)
+
+  return requestObj.promise.then(resp => {
+    // 假设 request.js 已经 JSON.parse(resp.body)
+    if (resp.body.code !== 200) {
+      throw new Error(resp.body.message || 'API 请求失败')
+    } else {
+      // API 成功, 返回 data 字段
+      return resp.body.data
+    }
+  }).catch(err => {
+    // httpFetch 内部的 catch 会处理 ETIMEDOUT 等
     return Promise.reject(err)
   })
 }
